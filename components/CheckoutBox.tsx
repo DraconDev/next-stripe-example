c5a4d1748170b6fd21e5cd3184bd2888cff18647
DraconDev <dracsharp@gmail.com>
2023-12-16 08:22:42 +0000

    upd structure

--- components/CheckoutBox.tsx
+++ components/CheckoutBox.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { generatePaymentIntentWithCart } from "@/utils/generatePaymentIntentWithCart";
+import { CartItem } from "@/types/cart";
+
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect } from "react";
@@ -8,6 +9,25 @@ import CheckoutForm from "./CheckoutForm";
 const stripePromise = loadStripe(
     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ""
 );
+
+export async function generatePaymentIntentWithCart(cartItems: CartItem[]) {
+    try {
+        const response = await fetch("/api/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ cartItems: cartItems }),
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        return data.clientSecret;
+    } catch (error) {
+        throw error;
+    }
+}
 
 const CheckoutWrapper = ({ children }: { children?: React.ReactNode }) => {
     const [clientSecret, setClientSecret] = React.useState("");
